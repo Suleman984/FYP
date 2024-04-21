c52fae6cd1f0bbc3dc6b02fc68d37c0a3eeaf789
Suleman <asma.liaquat@tangenttek.com>
2024-04-21 12:57:07 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginScreen from './Components/Authentication/Login/LoginScreen';
 import Signup from './Components/Authentication/SignUp/SignupScreen';
 import Authentication from './Components/Authentication/Authentication';
 import Homepage from './Components/HomePage/HomepageComponents/Homepage'
 import DataComponent from './Components/ExploreBusiness/Datacomponent';
+
+
 function App() {
+
+  useEffect(() => {
+    const dataFetch=fetch('http://192.168.18.17:3000/get-scrapped-data')
+    console.log(dataFetch)
+  }, [])
   return (
     <div className="App">
       {/* <LoginScreen/> */}
       {/* <Homepage/> */}
       <DataComponent/>
+      
     </div>
   );
 }
