0cd12f07361b538c3c8413e7ed3b601b0aa8f578
Suleman <asma.liaquat@tangenttek.com>
2024-06-04 00:10:29 +0500

    updated ip

--- src/Components/ExploreBusiness/Datacomponent.jsx
+++ src/Components/ExploreBusiness/Datacomponent.jsx
@@ -17,7 +17,7 @@ const DataComponent = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://192.168.0.190:3001/get-scrapped-data');
+        const response = await axios.get('http://192.168.18.17:3001/get-scrapped-data');
         setDataArray(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
--- src/Components/WebsiteAnalytics/InputDomain.jsx
+++ src/Components/WebsiteAnalytics/InputDomain.jsx
@@ -20,60 +20,36 @@ const UrlInputForm = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get('http://192.168.0.190:3001/get-analytics-page', { params: { url: businessName } });
+        const res = await axios.get('http://192.168.18.17:3001/get-analytics-page', { params: { url: businessName } });
         setResponse(res.data);
         
 		// **************************************************************************************
-        const parser = new DOMParser();
+    //     const parser = new DOMParser();
 
-			// Parse the HTML string into a DOM document
-			const doc = parser.parseFromString(res.data, 'text/html');
+		// 	const doc = parser.parseFromString(res.data, 'text/html');
 
-			// Select the table element from the document
-			const table = doc.querySelector('table');
+		// 	const table = doc.querySelector('table');
 
-			// Now you can work with the table element as needed
-			// console.log(table);
+		// 	const headings = Array.from(table.querySelectorAll('th')).map(th => th.textContent.trim());
+		// 	const rows = Array.from(table.querySelectorAll('tbody tr'));
 
-			const headings = Array.from(table.querySelectorAll('th')).map(th => th.textContent.trim());
-			const rows = Array.from(table.querySelectorAll('tbody tr'));
-			// console.log(headings, rows)
+		// 	const extractedData = rows.map(row => {
+		// 		const rowData = Array.from(row.querySelectorAll('td')).map(td => td.textContent.trim());
+		// 		const obj = {};
+		// 		headings.forEach((heading, index) => {
+		// 			obj[heading] = rowData[index];
+		// 		});
+		// 		return obj;
+		// 	});
+		// 	const cardDivs = doc.querySelectorAll('.grid > div');
+		
+		// 	const dataArray = [];
 
-			const extractedData = rows.map(row => {
-				const rowData = Array.from(row.querySelectorAll('td')).map(td => td.textContent.trim());
-				const obj = {};
-				headings.forEach((heading, index) => {
-					obj[heading] = rowData[index];
-				});
-				return obj;
-			});
-
-			// console.log(extractedData);
-
-			//  table format parsing  end
-
-
-
-			// Select all card divs
-			const cardDivs = doc.querySelectorAll('.grid > div');
-
-			// Initialize an empty array to store the extracted data
-			const dataArray = [];
-
-			// Iterate over each card div
-			cardDivs.forEach(cardDiv => {
-				// Select the first and second <p> elements inside the card div
-				const heading = cardDiv.querySelector('p:nth-child(1)').textContent.trim();
-				const value = cardDiv.querySelector('p:nth-child(2)').textContent.trim();
-
-				// Push an object with heading and value properties into the dataArray
-				dataArray.push({ heading, value });
-			});
-
-			// Log the resulting array of objects
-			console.log(dataArray);
-		// **************************************************************************************
-
+		// 	cardDivs.forEach(cardDiv => {
+		// 		const heading = cardDiv.querySelector('p:nth-child(1)').textContent.trim();
+		// 		const value = cardDiv.querySelector('p:nth-child(2)').textContent.trim();
+		// 		dataArray.push({ heading, value });
+		// 	});
 
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -84,7 +60,6 @@ const UrlInputForm = () => {
       fetchData();
     }
   }, [businessName]);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (url) {
@@ -115,12 +90,7 @@ const UrlInputForm = () => {
           <p>{businessName}</p>
         </div>
       )}
-      {/* {response && (
-        <div style={{ width: '100%', wordWrap: 'break-word', whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
-          <h2>API Response:</h2>
-          <pre>{JSON.stringify(response, null, 2)}</pre>
-        </div>
-      )} */}
+      
        {response && (
         <div style={{ width: '100%' }}>
           <h2>API Response:</h2>
